47464c76479344858a0aa2434749b80b4cbe1352
Chang Ye <yech1990@gmail.com>
2024-06-25 02:30:11 -0500

    quick update

---
+++ setup.py
@@ -0,0 +1,103 @@
+import os
+import platform
+import shutil
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution, Extension
+
+from Cython.Build import cythonize
+
+# Define the base path for minimap2 sources
+minimap2_base = "coralsnake/minimap2"
+
+# Define extra compile arguments based on the platform
+extra_compile_args = ["-DHAVE_KALLOC", "-O3"]
+include_dirs = [minimap2_base]
+
+# Adjust compile args for ARM or x86 architectures
+if platform.machine() in ["aarch64", "arm64"]:
+    include_dirs.append(os.path.join(minimap2_base, "sse2neon"))
+    extra_compile_args.extend(["-ftree-vectorize", "-DKSW_SSE2_ONLY", "-D__SSE2__"])
+else:
+    extra_compile_args.append("-msse4.1")  # Note: ancient x86_64 CPUs don't have SSE4
+
+libraries = ["z", "m", "pthread"]
+
+
+def build():
+    sources = [
+        "python/mappy.pyx",
+        "align.c",
+        "bseq.c",
+        "lchain.c",
+        "seed.c",
+        "format.c",
+        "hit.c",
+        "index.c",
+        "pe.c",
+        "options.c",
+        "ksw2_extd2_sse.c",
+        "ksw2_exts2_sse.c",
+        "ksw2_extz2_sse.c",
+        "ksw2_ll_sse.c",
+        "kalloc.c",
+        "kthread.c",
+        "map.c",
+        "misc.c",
+        "sdust.c",
+        "sketch.c",
+        "esterr.c",
+        "splitidx.c",
+    ]
+    depends = [
+        "minimap.h",
+        "bseq.h",
+        "kalloc.h",
+        "kdq.h",
+        "khash.h",
+        "kseq.h",
+        "ksort.h",
+        "ksw2.h",
+        "kthread.h",
+        "kvec.h",
+        "mmpriv.h",
+        "sdust.h",
+        "python/cmappy.h",
+        "python/cmappy.pxd",
+    ]
+    sources = [os.path.join(minimap2_base, file) for file in sources]
+    depends = [os.path.join(minimap2_base, file) for file in depends]
+
+    extensions = [
+        Extension(
+            "mappy",
+            sources=sources,
+            depends=depends,
+            extra_compile_args=extra_compile_args,
+            include_dirs=include_dirs,
+            libraries=libraries,
+        )
+    ]
+    ext_modules = cythonize(
+        extensions,
+        include_path=include_dirs,
+        compiler_directives={"binding": True, "language_level": 3},
+    )
+
+    distribution = Distribution({"name": "coralsnake", "ext_modules": ext_modules})
+    distribution.package_dir = "coralsnake"
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project directory
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
