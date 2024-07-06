3cd2a2c713af1dc8e6fd48b54ed3cac4495dfc64
Chang Ye <yech1990@gmail.com>
2024-07-05 20:02:41 -0500

    quick update

--- coralsnake/genegroup.py
+++ coralsnake/genegroup.py
@@ -171,6 +171,7 @@ def rename_snRNA(gene_name):
     # RNVU1-1 -> U1V
     # .., etc
     # meanwhile
+    gene_name = re.sub(r"RNU(\d+)$", r"U\1", gene_name)
     gene_name = re.sub(r"RNU(\d+)-\d+.*", r"U\1", gene_name)
     gene_name = re.sub(r"RNU(\d+[A-Z]+)-\d+.*", r"U\1", gene_name)
     gene_name = re.sub(r"RNU(\d+)ATAC.*", r"U\1ATAC", gene_name)
