03ce531a1f9fcd1d68c908a220e7a87deb6db977
Chang Ye <yech1990@gmail.com>
2026-03-12 20:32:53 +0800

    perf: add aggressive O3, LTO and native architecture flags to C build

--- build_ext/build_seqops.py
+++ build_ext/build_seqops.py
@@ -1,4 +1,23 @@
 from setuptools import setup, Extension
+import platform
+
+def get_compile_args():
+    args = [
+        "-O3", 
+        "-funroll-loops", 
+        "-ffast-math", 
+        "-DNDEBUG",
+        "-fomit-frame-pointer",
+        "-flto",
+    ]
+    # Add architecture-specific flags
+    machine = platform.machine().lower()
+    if machine in ["x86_64", "amd64"]:
+        args.append("-march=native")
+        args.append("-msse4.2") # Standard for modern bioinformatics
+    elif machine in ["arm64", "aarch64"]:
+        args.append("-march=native")
+    return args
 
 setup(
     name="seqops",
@@ -7,6 +26,8 @@ setup(
             "coralsnake.seqops",
             ["coralsnake/seqops.c"],
             libraries=["z"],
+            extra_compile_args=get_compile_args(),
+            extra_link_args=["-flto"],
         )
     ],
 )
